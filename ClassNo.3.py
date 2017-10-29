618f726b863463232affb00bbdaeecea5e7c29f9
Furumori Akito <james62006@gmail.com>
2017-10-29 15:13:26 +0800

    ClassNo.3

---
+++ ClassNo.3.py
@@ -0,0 +1,22 @@
+#變數
+num=5566
+#num就是變數,這邊就是把變數指向5566,變數名稱=代表對象
+print("I love",num)
+#"="是指派不是相等
+num=7788
+print("I love",num)
+num=5566
+print("I hate",num)
+#程式是由上往下執行的,所以跑出來不是1234
+print(num)
+num=1234
+#有順序之分
+
+#變數名稱有英文大小寫分別,數字還有底線,注意裡面不能有@%&$的符號
+num=1
+Num=2
+nUm=3
+print(num,Num,nUm)
+
+
+
